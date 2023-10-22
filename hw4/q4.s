87ba27bf930b84dbcaa4ecf0a1e536b1b9a6b3fc
Varsha Rathore <63990614+varsharathore16@users.noreply.github.com>
2023-10-22 11:48:04 -0400

    Update and rename q4.s to hw4/q4.s

--- hw4/q4.s
+++ hw4/q4.s
@@ -41,3 +41,39 @@ exit:   addi    a7, x0, 10
 # -1:   "4294967295"
 # -3666:   "4294963630"
 uint2decstr:
+	addi sp, sp, -8 # creates space on stack for ra and s0
+	sw ra, 4(sp)
+	sw s0, 0(sp)
+	li t0, 10# loads 10 into t0
+	li t1, 0 # register initialization
+	mv t2, a0 # register initialization
+	mv t3, a1 # register initialization
+
+test:
+	bge t3, t0, recursive #checks if t3 >= t0 (t0 is 10) and goes to recursive function
+	addi t3, t3, '0'
+	sb t3, 0(t2)
+	addi t1, t1, 1 #increments length by 1
+	addi t2, t2, 1 #increments pointer by 1
+	li t3, 0 # resets t3 to 0
+	sb zero, 0(t2)
+	addi t2, t2, -1
+	mv a0, t2
+	lw ra, 4(sp)
+	lw s0, 0(sp)
+	addi sp, sp, 8
+	ret
+
+recursive:
+	divu t4, t3, t0 # divides t3 by 10
+	mv a0, t2 #
+	mv a1, t4
+	jal ra, uint2decstr
+	mv t2, a0
+	remu t3, t3, t0
+	addi t3, t3, '0' # adds 0 to value
+	sb t3, 0(t2) # stored as byte
+	addi t1, t1, 1 #increments length by
+	addi t2, t2, 1 #increments pointer by 1
+	li t3, 0 # resets t3 to 0
+	j test # jumps to the test function
